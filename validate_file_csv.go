8b4655051c8aa2a79229892e404811b4ce893dd3
alvarossantos <alvaroguimaraessantos@gmail.com>
2025-10-22 11:56:53 -0300

    Updating Main and Creating Validate CSV

---
+++ validate_file_csv.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func validateFileCSV(path string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return errors.New("file not exist")
+		} else {
+			return fmt.Errorf("access error : %v", err)
+		}
+	}
+
+	if info.IsDir() {
+		return errors.New("is a directory")
+	}
+
+	if info.Size() == 0 {
+		return errors.New("file is empty")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	csv := csv.NewReader(file)
+	expectedCols := 0
+	for {
+		read, err := csv.Read()
+		if err == io.EOF {
+			break
+		}
+		
+		if err != nil {
+			return err
+		}
+
+		if expectedCols == 0 {
+			expectedCols = len(read)
+		} else {
+			if len(read) != expectedCols {
+				return fmt.Errorf("inconsistent number of columns: expected %d, got %d", expectedCols, len(read))
+			}
+		}
+	}
+
+	return nil
+}

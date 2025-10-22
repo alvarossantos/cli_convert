fdd44f99ff715976ad9cd3ce27138cf5dbb92b01
alvarossantos <alvaroguimaraessantos@gmail.com>
2025-10-22 09:19:03 -0300

    updating main and creating the validate json

---
+++ validate_file_json.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io"
+	"os"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+func validateFileJSON(path string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return errors.New("file not exist")
+		}else {
+			return fmt.Errorf("access error: %v", err)
+		}
+	}
+	
+	if info.IsDir() {
+		return errors.New("is a directory")
+	}
+
+	if info.Size() == 0 {
+		return errors.New("file is empty")
+	}
+	
+	
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	
+	read, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if !json.Valid(read) {
+		return errors.New("invalid json")
+	}
+	return nil
+}
